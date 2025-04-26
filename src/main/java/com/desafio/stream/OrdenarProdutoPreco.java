d827f9b7ce5339ba5e79d81ebcc47854a56c62c4
rafael grando <sta_rafinha@hotmail.com>
2025-04-26 12:06:56 -0300

    Feat:Exemplos de Stream

---
+++ src/main/java/com/desafio/stream/OrdenarProdutoPreco.java
@@ -0,0 +1,12 @@
+package com.desafio.stream;
+
+import java.util.Comparator;
+
+class OrdenarProdutoPreco implements Comparator<Produto> {
+
+  @Override
+  public int compare(Produto p1, Produto p2) {
+    return p1.getPreco().compareTo(p2.getPreco());
+  }
+
+}
