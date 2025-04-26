ff0ccee8eeba88b571260ff93f46afd03c742517
rafael grando <sta_rafinha@hotmail.com>
2025-04-26 12:06:36 -0300

    Feat:Exemplo de interface funcional e Classe anonima

---
+++ src/main/java/com/desafio/functionalinterface/InterfaceFunctional.java
@@ -0,0 +1,14 @@
+package com.desafio.functionalinterface;
+
+@FunctionalInterface
+public interface InterfaceFunctional {
+  void fazQualquerCoisa(String facaAlgo);
+
+  default void fazQualquerCoisa2() {
+    System.out.println("Faz qualquer coisa 2");
+  }
+
+  static void fazQualquerCoisa3() {
+    System.out.println("Faz qualquer coisa 3");
+  }
+}
