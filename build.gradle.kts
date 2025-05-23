f6bff0a3052e2c6ea1418686c71b0cf426c52008
rafael grando <sta_rafinha@hotmail.com>
2025-05-23 17:10:52 -0300

    Projeto criado

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    kotlin("jvm")
 }
 
 group = "com.desafio"
@@ -12,8 +13,21 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.assertj:assertj-core:4.0.0-M1")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.19.0")
+    //add dependnecy Lombok
+    compileOnly("org.projectlombok:lombok:1.18.30")
+    annotationProcessor("org.projectlombok:lombok:1.18.30")
+
+    // Apache POI for Excel file generation
+    implementation("org.apache.poi:poi:5.4.1")
+    implementation("org.apache.poi:poi-ooxml:5.4.1")
+    implementation(kotlin("stdlib-jdk8"))
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(21)
 }
